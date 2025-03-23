7d96228b1db1aa81152ab1a91356426248979bc8
Rio Ragil Ramdani <rioragilramdani7@gmail.com>
2025-03-24 00:06:43 +0700

    feat: Tambah AddBlog screen ke navigation

--- app/src/main/java/com/rioramdani0034/assesment1/navigation/Screen.kt
+++ app/src/main/java/com/rioramdani0034/assesment1/navigation/Screen.kt
@@ -3,4 +3,6 @@ package com.rioramdani0034.assesment1.navigation
 sealed class Screen(val route: String) {
     data object Home : Screen("mainScreen")
     data object About : Screen("aboutScreen")
+    data object AddBlog : Screen("addBlogScreen")
+
 }
